361f501552948bea0f3fb712797c440890637ff9
0qtttt <pyg7508@gmail.com>
2018-04-18 21:51:38 +0900

    prompt loadinng

--- src/pages/component/component.ts
+++ src/pages/component/component.ts
@@ -1,5 +1,16 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams, ActionSheetController, Platform, ModalController} from 'ionic-angular';
+import {
+  ActionSheetController,
+  AlertController,
+  IonicPage,
+  LoadingController,
+  ModalController,
+  NavController,
+  NavParams,
+  Platform,
+  ToastController
+} from 'ionic-angular';
+import {AccountInterface} from "../../interfaces/account";
 
 /**
  * Generated class for the ComponentPage page.
@@ -14,12 +25,17 @@ import {IonicPage, NavController, NavParams, ActionSheetController, Platform, Mo
   templateUrl: 'component.html'
 })
 export class ComponentPage {
+  private accountData = {} as AccountInterface
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public actionSheetCrtl: ActionSheetController,
     public platform: Platform,
-    public modalCtrl: ModalController
+    public modalCtrl: ModalController,
+    public alertCtrl: AlertController,
+    public toastCtrl: ToastController,
+    public loadingCtrl: LoadingController
   ) {
   }
 
@@ -69,5 +85,45 @@ export class ComponentPage {
 
   slide() {
     this.navCtrl.push('SlidePage')
+  }
+
+  promptAlert() {
+    let prompt = this.alertCtrl.create({
+        title: 'Login',
+        message: "이름과 E-Mail를 입력하세요",
+        inputs: [{name: 'name', placeholder: 'Name 입력'}, {name: 'email', placeholder: 'Email 입력'},],
+        buttons: [{
+          text: '취소', handler: data => {
+            console.log('Cancel clicked');
+          }
+        }, {
+          text: '저장', handler: data => {
+            this.accountData = {name: data.name, email: data.email}
+            this.navCtrl.push('navPage', {account: this.accountData});
+          }
+        }]
+      })
+    ;
+    prompt.present();
+  }
+
+  toast() {
+    let toast = this.toastCtrl.create({
+      message: '3초 동안 보였다가 사라집니다.',
+      duration: 3000,
+      position: 'top',
+      showCloseButton: true,
+      closeButtonText: 'ggpgpg'
+    });
+    toast.present()
+  }
+
+  loading() {
+    let loading = this.loadingCtrl.create();
+    loading.present();
+
+    setTimeout(() => {
+      loading.dismiss()
+    }, 3000)
   }
 }
---
+++ src/pages/nav/nav.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {AccountInterface} from "../../interfaces/account";
+
+/**
+ * Generated class for the NavPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage({name: 'navPage'})
+@Component({
+  selector: 'page-nav',
+  templateUrl: 'nav.html',
+})
+export class NavPage {
+  private accountData = {} as AccountInterface;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.accountData = this.navParams.get('account')
+    console.log('ionViewDidLoad NavPage');
+  }
+
+}
