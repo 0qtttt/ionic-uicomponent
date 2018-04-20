44f6aa6a3858bf185cb948bb3800692bc981b7e1
0qtttt <pyg7508@gmail.com>
2018-04-20 21:38:39 +0900

    Email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {EmailComposer} from "@ionic-native/email-composer";
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    EmailComposer,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/nav/nav.ts
+++ src/pages/nav/nav.ts
@@ -16,12 +16,27 @@ import {AccountInterface} from "../../interfaces/account";
 })
 export class NavPage {
   private accountData = {} as AccountInterface;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private emailComposer: EmailComposer
+  ) {
   }
 
   ionViewDidLoad() {
     this.accountData = this.navParams.get('account')
     console.log('ionViewDidLoad NavPage');
   }
+
+  sendEmail(item: ItemSliding, user) {
+    item.close();
+    let email = {
+      to: user.email,
+      subject: 'To :' + user.name,
+      body: '',
+      isHtml: true
+    };
+// Send a text message using default options
+    this.emailComposer.open(email);
+  }
+
 
 }
